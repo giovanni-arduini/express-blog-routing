302bfdb3192aeaa654e23d1204aac1360328b2be
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-19 15:54:48 +0100

    ADD app.use for posts and postsRouter

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log("Hello node!");
 const express = require("express");
 const app = express();
 const port = 3000;
+const postsRouter = require("./router/posts");
 
 app.use(express.static("public"));
 
@@ -10,6 +11,8 @@ app.get("/", (req, res) => {
   //   console.log("root");
   res.send("Hello world!");
 });
+
+app.use("/posts", postsRouter);
 
 app.listen(port, () => {
   console.log("Server is listening!");
