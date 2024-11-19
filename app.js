e9320e2636c23b9c5494af3cda8866897cae5a13
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-19 15:33:52 +0100

    ADD CRUD index

--- app.js
+++ app.js
@@ -7,9 +7,25 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  console.log("root");
+  //   console.log("root");
   res.send("Hello world!");
 });
+
+// CRUD
+
+//index
+app.get("/posts", (req, res) => {
+  res.send("Ecco la lista dei post");
+});
+//show
+
+//store
+
+//update
+
+//modify
+
+//destroy
 
 app.listen(port, () => {
   console.log("Server is listening!");
