5682e04d4c4798a20d64359332615fe97b935cbd
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-19 15:49:23 +0100

    ADD routes in router/posts.js

--- routers/posts.js
+++ routers/posts.js
@@ -1,2 +1,42 @@
 const express = require("express");
 const router = express.Router();
+// Non c'è più l'istanza dell'applicativo app, ma router!
+
+// Qui salvo tutte le rotte!
+// CRUD
+
+//index
+router.get("/", (req, res) => {
+  res.send("Ecco la lista dei post");
+});
+
+//show
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Ecco il post con id ${id}`);
+});
+
+//store
+router.post("/", (req, res) => {
+  res.send("Crea un nuovo post!");
+});
+
+//update
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Aggiorna il post con id ${id}`);
+});
+
+//modify
+router.patch("/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Modifica il post con id ${id}`);
+});
+
+//destroy
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Elimina il post con id ${id}`);
+});
+
+module.exports = router;
