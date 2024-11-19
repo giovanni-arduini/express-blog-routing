d466a398629fe659a986fa4f8b1051a272e26665
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-19 15:49:38 +0100

    FIX moved routes from app.js

--- app.js
+++ app.js
@@ -11,42 +11,6 @@ app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
-// CRUD
-
-//index
-app.get("/posts", (req, res) => {
-  res.send("Ecco la lista dei post");
-});
-
-//show
-app.get("/posts/:id", (req, res) => {
-  const id = req.params.id;
-  res.send(`Ecco il post con id ${id}`);
-});
-
-//store
-app.post("/posts", (req, res) => {
-  res.send("Crea un nuovo post!");
-});
-
-//update
-app.put("/posts/:id", (req, res) => {
-  const id = req.params.id;
-  res.send(`Aggiorna il post con id ${id}`);
-});
-
-//modify
-app.patch("/posts/:id", (req, res) => {
-  const id = req.params.id;
-  res.send(`Modifica il post con id ${id}`);
-});
-
-//destroy
-app.delete("/posts/:id", (req, res) => {
-  const id = req.params.id;
-  res.send(`Elimina il post con id ${id}`);
-});
-
 app.listen(port, () => {
   console.log("Server is listening!");
 });
